f022c03cd32f0134e3546ec7c2afa4f0f8099ffd
Charu Priya <charu_priya@yomabank.net>
2018-10-13 20:08:56 +0530

    added linter

--- src/middleware/test/api-framework.test.js
+++ src/middleware/test/api-framework.test.js
@@ -5,7 +5,7 @@ const api = require('../api-framework')
 
 describe('API framework', () => {
   it('Should be able to call weather api and return 200 res', async () => {
-    var res = await api({
+    const res = await api({
       url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
       method: 'GET'
     });
