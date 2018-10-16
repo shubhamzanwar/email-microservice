f88a0ed6df7532104f27631b1f4356d4df550ae7
lakhara <siddharth.lakhara@gmail.com>
2018-10-16 11:35:37 +0530

    added input validations

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const Joi = require('joi');
+const { body } = require('express-validator/check');
 
 const router = express.Router();
 
@@ -17,10 +19,22 @@ const sendEmails = (toAdress, subject, message) => transporter.sendMail({
   text: message
 })
 
+const emailRequestSchema = {
+  toAdress: Joi.string().email().required().error(new Error('recipients address is required')),
+  subject: Joi.string().optional(),
+  message: Joi.string().min(1).required().error(new Error('Message body is required'))
+}
+
 router.post('/', async (req, res) => {
-  const { toAdress, subject, message } = req.body;
-  await sendEmails(toAdress, subject, message);
-  res.send('Email sent');
+  const validation = Joi.validate(req.body, emailRequestSchema, { abortEarly: false });
+  if (!validation.error) {
+    const { toAdress, subject, message } = req.body;
+    await sendEmails(toAdress, subject, message);
+    res.send('Email sent');
+  } else {
+    res.status(400);
+    res.send(validation.error.message);
+  }
 });
 
 module.exports = router;
--- src/routes/tests/email.spec.js
+++ src/routes/tests/email.spec.js
@@ -14,4 +14,42 @@ describe('Testing email functionality', () => {
       done();
     });
   });
+
+  test('input validations: requires toAdress', (done) => {
+    const postOptions = {
+      message: 'This mail was sent while testing',
+    };
+    request(app).post('/email').send(postOptions).then((response) => {
+    //   console.log(response.statusCode);
+    //   console.log(response.text);
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toBe('recipients address is required');
+      done();
+    });
+  });
+
+  test('input validations: rejects invalid email', (done) => {
+    const postOptions = {
+      toAdress: 'siddharth.lakhara',
+      message: 'This mail was sent while testing',
+    };
+    request(app).post('/email').send(postOptions).then((response) => {
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toBe('recipients address is required');
+      done();
+    });
+  });
+
+  test('input validations: requires message', (done) => {
+    const postOptions = {
+      toAdress: 'siddharth.lakhara@gmail.com',
+    };
+    request(app).post('/email').send(postOptions).then((response) => {
+      //   console.log(response.statusCode);
+      //   console.log(response.text);
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toBe('Message body is required');
+      done();
+    });
+  });
 })
