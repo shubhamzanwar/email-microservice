e5078d37d6ddd4a02c7f04efdd14c33fc53c8a09
ShubhamZanwar <zanwar.shubham@gmail.com>
2018-10-16 12:34:29 +0530

    email route: Using the sendEmail Handler

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,40 +1,7 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const Joi = require('joi');
-const config = require('../config/index');
+const { emailHandler } = require('../handlers/sendEmail');
 
 const router = express.Router();
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: config.email.username,
-    pass: config.email.password,
-  },
-});
-
-const sendEmails = (toAdress, subject, message) => transporter.sendMail({
-  to: toAdress,
-  subject,
-  text: message
-})
-
-const emailRequestSchema = {
-  toAdress: Joi.string().email().required().error(new Error('recipients address is required')),
-  subject: Joi.string().optional(),
-  message: Joi.string().min(1).required().error(new Error('Message body is required'))
-}
-
-router.post('/', async (req, res) => {
-  const validation = Joi.validate(req.body, emailRequestSchema, { abortEarly: false });
-  if (!validation.error) {
-    const { toAdress, subject, message } = req.body;
-    await sendEmails(toAdress, subject, message);
-    res.send('Email sent');
-  } else {
-    res.status(400);
-    res.send(validation.error.message);
-  }
-});
+router.post('/', emailHandler);
 
 module.exports = router;
